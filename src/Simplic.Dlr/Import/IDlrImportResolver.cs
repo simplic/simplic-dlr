31dddea9277f1f5ba7f61ab2ba6aa630d140b2ec
beggers <be@spiegelburg.net>
2016-01-28 11:55:55 +0100

    Add GetType

--- src/Simplic.Dlr/Import/IDlrImportResolver.cs
+++ src/Simplic.Dlr/Import/IDlrImportResolver.cs
@@ -7,6 +7,27 @@ using System.Text;
 namespace Simplic.Dlr
 {
     /// <summary>
+    /// Resolve path
+    /// </summary>
+    public enum ResolvedType
+    {
+        /// <summary>
+        /// Could not detect/find script
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Package resolved
+        /// </summary>
+        Package = 1,
+
+        /// <summary>
+        /// Module resolved
+        /// </summary>
+        Module = 2
+    }
+
+    /// <summary>
     /// Resolver for importing scripts and modules
     /// </summary>
     public interface IDlrImportResolver
@@ -14,9 +35,16 @@ namespace Simplic.Dlr
         /// <summary>
         /// Import path
         /// </summary>
-        /// <param name="path"></param>
+        /// <param name="path">Returns the script-source</param>
         /// <returns></returns>
         string GetScriptSource(string path);
+
+        /// <summary>
+        /// Detect the type of the script. Returns None-Type if not found
+        /// </summary>
+        /// <param name="path">Module name, package path or script path (ends with .py)</param>
+        /// <returns>Type of the script</returns>
+        ResolvedType GetType(string path);
 
         /// <summary>
         /// Unique resolver name
